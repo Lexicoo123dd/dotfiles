86361fc76ffb41a1d22a39fee22c4cdbd743ac3a
Lexicoo <allundva@gmail.com>
2025-11-16 19:41:04 +0100

    autocomplete

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -7,5 +7,18 @@ return {
     },
     {
         'machakann/vim-sandwich',
+    },
+    {
+        'saghen/blink.cmp',
+        dependencies = { 'rafamadriz/friendly-snippets' },
+
+        version = '1.*',
+        opts = {
+            keymap = { preset = 'default' },
+
+            appearance = {
+                nerd_font_variant = 'mono'
+            },
+        },
     }
 }
