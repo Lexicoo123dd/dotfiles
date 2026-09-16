b7a9936ec33a44a9512f27a4eefd2d3dac91a9a9
Lexicoo <allundva@gmail.com>
2026-09-16 22:36:16 +0200

    Treesitter languages

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -12,6 +12,18 @@ return {
             enable_transparency()
         end
     },
+    -- {
+    --     "RedsXDD/neopywal.nvim",
+    --     name = "neopywal",
+    --     lazy = false,
+    --     priority = 1000,
+    --     opts = {},
+    --     config = function()
+    --         local neopywal = require("neopywal")
+    --         neopywal.setup()
+    --         vim.cmd.colorscheme("neopywal")
+    --     end
+    -- },
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
